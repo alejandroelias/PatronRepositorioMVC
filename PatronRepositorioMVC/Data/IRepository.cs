1cd164d54660e881f9ff6bff52f7294c4f5cf8ed
Alejandro Elias <alejandro.elias@outlook.com>
2023-07-16 08:16:11 -0600

    Sin cambios desde ultima modificacion

---
+++ PatronRepositorioMVC/Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace PatronRepositorioMVC.Data
+{
+    interface IRepository<T>
+    {
+        //CRUD
+        void Add(T model);
+        void Delete(int modelID);
+        void Update(T model);
+        void Count(Expression<Func<T,bool>> where);
+        T GetByID(int modelID);
+        //IEnumerable<T> FindBy(ParametersForQuery<T> parametersForQuery);
+    }
+}
---
+++ PatronRepositorioMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PatronRepositorioMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PatronRepositorioMVC")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("11431f89-2ccb-4f01-b444-dc9bf05d9057")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
